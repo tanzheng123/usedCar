a4361ad086bcaf08a624916a339de1d27ea63c0c
unknown <you@example.com>
2020-11-12 13:26:16 +0800

    对代码进行修改

--- src/main/java/com/yajun/SpringbootApplication.java
+++ src/main/java/com/yajun/SpringbootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootApplication /*extends SpringBootServletInitializer */{
 
 	public static void main(String[] args) {
+		System.out.println("fffffffffffffffffffff");
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
